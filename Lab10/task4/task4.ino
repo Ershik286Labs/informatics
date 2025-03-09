791bb777984aaa3fa327d43f5cd06895b142d303
Ershik286Labs <ershov.ershik286@gmail.com>
2025-03-10 01:44:04 +0300

    Update task4.ino

--- Lab10/task4/task4.ino
+++ Lab10/task4/task4.ino
@@ -1,247 +1,50 @@
-int soundPin = 3;
-int Button = 2;
-int ledPin[3] = {7, 6, 5};
-int dotDelay = 50;
-int countWord = 0;//выбранное сообщение
-long time;
-int rate = 100;
+#define FIRST_LEFT_PIN 4
+#define FIRST_RIGHT_PIN 11
+#define BAR_COUNT 14
+#define MAX_SCORE 28
+#define Red A5
+#define Blue A4
+#define Green 0
 
-const int c = 261;
-const int d = 294;
-const int e = 329;
-const int f = 349;
-const int g = 391;
-const int gS = 415;
-const int a = 440;
-const int aS = 455;
-const int b = 466;
-const int cH = 523;
-const int cSH = 554;
-const int dH = 587;
-const int dSH = 622;
-const int eH = 659;
-const int fH = 698;
-const int fSH = 740;
-const int gH = 784;
-const int gSH = 830;
-const int aH = 880;
+//A0 - 14, A3 - 17
+volatile int score = 0;
 
-unsigned long lastDebounceTime = 0;  // Время последнего корректного нажатия
-const unsigned long debounceDelay = 50; // Задержка для подавления дребезга (50 мс)
-
-
-void setup(){
-  pinMode(soundPin, OUTPUT);
-  pinMode(Button, INPUT_PULLUP);
-  for (int i = 0; i < 3; i++)
-    pinMode(ledPin[i], OUTPUT);  
-  attachInterrupt(digitalPinToInterrupt(Button), CheckPressed, FALLING); // Исправленный номер прерывания
-  Serial.begin(9600);
-}
-
-void beep(int note, int duration){
-  //Play tone on buzzerPin
-  if (countWord == 1) return;
-  tone(soundPin, note, duration);
-
-  //Play different LED depending on value of 'counter'
-  for (int i = 0; i < 3; i++)
-    digitalWrite(ledPin[i], HIGH);
-
-    delay(duration);
-  for (int i = 0; i < 3; i++)
-    digitalWrite(ledPin[i], LOW);
-
-  //Stop tone on buzzerPin
-  noTone(soundPin);
-
-  delay(50);
-
-  //Increment counter
-}
-
-void CheckPressed() {
-  Serial.println(countWord);
-  unsigned long currentTime = millis();
-  if ((currentTime - lastDebounceTime) > debounceDelay) {
-    noTone(soundPin);
-    countWord = (countWord + 1) % 2;
-    // Кнопка действительно нажата (а не просто дребезг)
-    lastDebounceTime = currentTime;
-  }
-}
-
-void loop() {
-  if (countWord == 0) {
-    firstSectionSW();
-    secondSectionSW();
-  }
-  else {
-    firstSectionPS();
-  }
-}
-
-void firstSectionSW()
+int scorePin = 4;
+int ledOne = 4;
+bool ButtonLast[2] = {false, false};
+int massiveLeftPin[] = {-24, -20, -16, -12, -8, -4, 0};
+int massiveRightPin[] = {0, 4, 8, 12, 16, 20, 24};
+void setup()
 {
-  beep(a, 500);
-  beep(a, 500);
-  beep(a, 500);
-  beep(f, 350);
-  beep(cH, 150);
-  beep(a, 500);
-  beep(f, 350);
-  beep(cH, 150);
-  beep(a, 650);
-
-  delay(500);
-
-  beep(eH, 500);
-  beep(eH, 500);
-  beep(eH, 500);
-  beep(fH, 350);
-  beep(cH, 150);
-  beep(gS, 500);
-  beep(f, 350);
-  beep(cH, 150);
-  beep(a, 650);
-
-  delay(500);
+  //Serial.begin(9600);
+  for (int i = 0; i < BAR_COUNT; ++i){
+    pinMode(i + FIRST_LEFT_PIN, OUTPUT); }
+  pinMode(Red, OUTPUT);
+  pinMode(Blue, OUTPUT);
+  pinMode(Green, OUTPUT);
+  attachInterrupt(INT0, pushP1, FALLING);
+  attachInterrupt(INT1, pushP2, FALLING);
 }
+void pushP1() { ++score; }
+void pushP2() { --score; }
 
-void secondSectionSW()
-{
-  beep(aH, 500);
-  beep(a, 300);
-  beep(a, 150);
-  beep(aH, 500);
-  beep(gSH, 325);
-  beep(gH, 175);
-  beep(fSH, 125);
-  beep(fH, 125);
-  beep(fSH, 250);
-
-  delay(325);
-
-  beep(aS, 250);
-  beep(dSH, 500);
-  beep(dH, 325);
-  beep(cSH, 175);
-  beep(cH, 125);
-  beep(b, 125);
-  beep(cH, 250);
-
-  delay(350);
-}
-
-void firstSectionPS()
-{
-    //BeeepSOV(soundPin, 174, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-    BeeepSOV(soundPin, 174, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 195, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 220, 499.9995, 555.555);
-    BeeepSOV(soundPin, 146, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 146, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 195, 499.9995, 555.555);
-    BeeepSOV(soundPin, 174, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 155, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 174, 499.9995, 555.555);
-    BeeepSOV(soundPin, 116, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 116, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 130, 499.9995, 555.555);
-    BeeepSOV(soundPin, 130, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 146, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 155, 499.9995, 555.555);
-    BeeepSOV(soundPin, 155, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 174, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 195, 499.9995, 555.555);
-    BeeepSOV(soundPin, 220, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 233, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 261, 749.99925, 833.3325);
-    BeeepSOV(soundPin, 174, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 293, 499.9995, 555.555);
-    BeeepSOV(soundPin, 261, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 233, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 261, 499.9995, 555.555);
-    BeeepSOV(soundPin, 174, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 174, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-	BeeepSOV(soundPin, 220, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 195, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 220, 499.9995, 555.555);
-    BeeepSOV(soundPin, 146, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 146, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 195, 499.9995, 555.555);
-    BeeepSOV(soundPin, 174, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 155, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 174, 499.9995, 555.555);
-    BeeepSOV(soundPin, 116, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 116, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-    BeeepSOV(soundPin, 220, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 195, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 174, 999.999, 1111.11);
-    BeeepSOV(soundPin, 293, 999.999, 1111.11);
-    BeeepSOV(soundPin, 261, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 220, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 261, 749.99925, 833.3325);
-    BeeepSOV(soundPin, 174, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 174, 999.999, 1111.11);
-    BeeepSOV(soundPin, 233, 999.999, 1111.11);
-    BeeepSOV(soundPin, 220, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 195, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 174, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 195, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 220, 749.99925, 833.3325);
-    BeeepSOV(soundPin, 146, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 146, 999.999, 1111.11);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-    BeeepSOV(soundPin, 195, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 220, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-    BeeepSOV(soundPin, 195, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 220, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-    BeeepSOV(soundPin, 195, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 233, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 311, 999.999, 1111.11);
-    BeeepSOV(soundPin, 311, 999.999, 1111.11);
-    BeeepSOV(soundPin, 293, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 261, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 261, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 293, 749.99925, 833.3325);
-    BeeepSOV(soundPin, 233, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 999.999, 1111.11);
-    BeeepSOV(soundPin, 261, 999.999, 1111.11);
-    BeeepSOV(soundPin, 233, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 220, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 195, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 220, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 233, 749.99925, 833.3325);
-    BeeepSOV(soundPin, 195, 249.99975, 277.7775);
-    BeeepSOV(soundPin, 195, 999.999, 1111.11);
-    BeeepSOV(soundPin, 233, 499.9995, 555.555);
-    BeeepSOV(soundPin, 220, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 195, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 174, 499.9995, 555.555);
-    BeeepSOV(soundPin, 116, 374.999625, 416.66625);
-    BeeepSOV(soundPin, 116, 124.999875, 138.88875);
-    BeeepSOV(soundPin, 174, 999.999, 1111.11);
-    BeeepSOV(soundPin, 195, 499.9995, 555.555);
-    BeeepSOV(soundPin, 220, 499.9995, 555.555);
-    BeeepSOV(soundPin, 233, 1999.998, 2222.22);
-}
-
-void BeeepSOV(int soundPin, int herc, float time, float delayTime) {
-  if (countWord == 0) return;
-  for (int i = 0; i < 3; i++) 
-    digitalWrite(ledPin[i], HIGH);
-  tone(soundPin, herc, time);
-  delay(delayTime);
-  for (int i = 0; i < 3; i++) 
-    digitalWrite(ledPin[i], LOW);
-  delay(100);
+void loop(){
+  digitalWrite(Green, HIGH);
+  delay(1000);
+  digitalWrite(Green, LOW);
+  while (abs(score) < MAX_SCORE) {
+    for (int i = 0; i < 7; ++i) {
+    	if (massiveLeftPin[i] >= score)
+          digitalWrite(i + FIRST_LEFT_PIN, HIGH);
+        else digitalWrite(i + FIRST_LEFT_PIN, LOW);
+    }
+    for (int i = 0; i < 7; i++) {
+        if (massiveRightPin[i] <= score)
+          digitalWrite(i + FIRST_RIGHT_PIN, HIGH);
+        else digitalWrite(i + FIRST_RIGHT_PIN, LOW);
+    }
+  }
+  if (score < 0) digitalWrite(Red, HIGH);
+  else digitalWrite(Blue, HIGH);
+  while (true) {}
 }
